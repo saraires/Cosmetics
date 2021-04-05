111200d4fc25e721a8c1cc6275fea3ddba8a706b
Sarai Restrepo <sarairestrepo22@gmail.com>
2021-04-05 16:54:59 -0500

    Dropdown Working

--- Frontend/src/pages/realizarCompra.js
+++ Frontend/src/pages/realizarCompra.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import axios from "../axios/axios";
 import { useState, useEffect } from "react";
+import { getFromLocal, saveToLocal } from '../functions/localstorage';
 import Menu from './menu';
 
 export default function RealizarCompra() {
 
     const [producto, setProducto] = useState([]);
+    const [cantidad, setCantidad] = useState([]);
 
     const today = new Date();
     const date = today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear();
@@ -13,7 +15,6 @@ export default function RealizarCompra() {
     useEffect(() => {
         axios.get(`/productos`)
             .then((res) => {
-                console.log(res.data);
                 setProducto(res.data);
             });
     }, []);
@@ -35,50 +36,56 @@ export default function RealizarCompra() {
             <Menu />
             <div className="container card" style={{ marginTop: "15px" }}>
                 <div className="mb-3" style={{ marginTop: "15px" }}>
-                    <label for="exampleFormControlInput1" className="form-label">Numero de orden</label>
+                    <label className="form-label">Numero de orden</label>
                     <input type="number" className="form-control" disabled id="exampleFormControlInput1" placeholder="Me traigo la info" />
                 </div>
                 <div className="mb-3">
-                    <label for="exampleFormControlTextarea1" className="form-label">Nombre</label>
+                    <label className="form-label">Nombre</label>
                     <input type="text" className="form-control" id="exampleFormControlTextarea1" rows="3"></input>
                 </div>
                 <div className="mb-3">
-                    <label for="exampleFormControlTextarea1" className="form-label">Fecha</label>
+                    <label className="form-label">Fecha</label>
                     <input type="number" className="form-control" disabled id="exampleFormControlTextarea1" placeholder={date} rows="3"></input>
                 </div>
+
+                <div class="dropdown">
+                    <button
+                        class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown button
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">
+                            Action
+                        </a>
+                        <a class="dropdown-item" href="#">
+                            Another action
+                        </a>
+                        <a class="dropdown-item" href="#">
+                            Something else here
+                        </a>
+                    </div>
+                </div>
+
                 <div>
                     {
                         producto.map((producto, index) => {
-                            console.log(producto.id_Producto)
                             return (
-                                <ul key={`${index - producto.id_Producto}`}>
-                                    <li width="10%">{producto.nombre}</li>
-                                    <li width="10%">{producto.precio}</li>
-                                    <li width="10%">{producto.cantidadDisponible}</li>
-                                </ul>
+
+                                <div className="form-check" key={index}>
+                                    <input className="form-check-input" type="radio" name="flexRadioDefault" onClick={() => saveToLocal("id_producto", producto.id_Producto)} />
+                                    <label className="form-check-label" for="flexRadioDefault1">{producto.nombre}</label>
+                                </div>
                             )
                         })
                     }
                 </div>
-                <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown button
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                        <li><a class="dropdown-item" href="#">Crema Facial</a></li>
-                        <li><a class="dropdown-item" href="#">Mascarilla</a></li>
-                        <li><a class="dropdown-item" href="#">Serum</a></li>
-                        <li><a class="dropdown-item" href="#">Jabon Facial</a></li>
-                        <li><a class="dropdown-item" href="#">Brocha maquillaje</a></li>
-                        <li><a class="dropdown-item" href="#">Bloqueador Solar</a></li>
-                        <li><a class="dropdown-item" href="#">Rubor</a></li>
-                        <li><a class="dropdown-item" href="#">Pestañina</a></li>
-                        <li><a class="dropdown-item" href="#">Pinta Uñas Azul</a></li>
-                        <li><a class="dropdown-item" href="#">Brillo labial</a></li>
-                    </ul>
+                <br />
+                <div className="mb-3">
+                    <label className="form-label">Cantidad del producto</label>
+                    <input type="number" className="form-control" rows="3"></input>
                 </div>
-
             </div>
-        </div>
+        </div >
+
     )
 }
